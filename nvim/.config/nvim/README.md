7c39ad12052cbd19154b50e9622fee669f528165
Aragon Joaquin <aragonjoaquin05@gmail.com>
2026-04-11 17:16:00 -0300

    Started nvim config from scratch on void

---
+++ nvim/.config/nvim/README.md
@@ -0,0 +1,5 @@
+# neovim from almost strach. only using the lazy template
+
+i will be updating my keys and scheisse.
+
+- leader: <SPACE>
---
+++ nvim/.config/nvim/lua/plugins/oil_nvim.lua
@@ -0,0 +1,45 @@
+return {
+  {
+    "stevearc/oil.nvim",
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {
+      view_options = {
+        show_hidden = true,
+      },
+      columns = {
+        "icon",
+        -- "permissions",
+        "size",
+        -- "mtime",
+      },
+
+      float = {
+        padding = 5,
+        max_width = 1280,
+        max_height = 720,
+        border = "rounded",
+        win_options = {
+          winblend = 0,
+        },
+        -- optionally override the oil buffers window title with custom function: fun(winid: integer): string
+        get_win_title = nil,
+        -- preview_split: Split direction: "auto", "left", "right", "above", "below".
+        preview_split = "right",
+        -- This is the config that will be passed to nvim_open_win.
+        -- Change values here to customize the layout
+        override = function(conf)
+          return conf
+        end,
+      },
+    },
+
+    dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    lazy = false,
+
+    keys = function()
+      vim.keymap.set("n", "<leader>o", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
+    end,
+  },
+}
